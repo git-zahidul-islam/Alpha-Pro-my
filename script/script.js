9f5ebe998f56b7e183ee60113d4a6310115bcc31
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-17 09:33:35 +0600

    60% is complete

---
+++ script/script.js
@@ -0,0 +1,17 @@
+function continueGame() {
+    const alphabet = randomNumberGenerator();
+    // alphabet showing place
+    const displayShow = document.getElementById('display-show');
+    displayShow.innerText = alphabet
+    // console.log('Display alphabet', alphabet);
+
+    // set background
+    setBackgroundColorById(alphabet);
+
+
+}
+function play() {
+    addElementById('home-screen');
+    removeElementById('play-ground')
+    continueGame()
+}
---
+++ script/utility.js
@@ -0,0 +1,32 @@
+// add hidden class
+function addElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+// remove hidden class
+function removeElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+// random alphabet generate
+function randomNumberGenerator() {
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabet = alphabetString.split('')
+    const index = Math.round(Math.random() * 25);
+    const random = alphabet[index];
+    return random;
+}
+// keyboard type
+function keyboard(id){
+    addEventListener('keyup',)
+}
+// set background color
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400')
+}
+// remove background color
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-400')
+}
